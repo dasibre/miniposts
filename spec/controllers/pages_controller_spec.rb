bfcf80f54e4e62716dcf9211bcd5cf42a4067629
James <james.naadjie@gmail.com>
2012-01-18 22:49:12 -0500

    site layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,19 +1,48 @@
 require 'spec_helper'
 
 describe PagesController do
-
+render_views
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+    
+    it "should have right title" do
+      get 'home'
+      response.should have_selector("title",
+                  :content => "Ruby on rails application | Home")
+                end
+    it "should have a non-black body" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
   end
+end
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
     end
+    
+     it "should have right title" do
+        get 'contact'
+        response.should have_selector("title",
+                    :content => "Ruby on rails application | Contact")
+                  end
   end
-
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    
+     it "should have right title" do
+        get 'about'
+        response.should have_selector("title",
+                    :content => "Ruby on rails application | About")
+                  end
+  end
+  
 end
