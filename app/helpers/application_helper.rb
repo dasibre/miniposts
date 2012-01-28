f8743e5226cfcc9b9bfaf6da2f5281fc01a2ba76
James <james.naadjie@gmail.com>
2012-01-27 23:41:08 -0500

    added users controller and layout tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,6 +10,6 @@ module ApplicationHelper
     end
     
   def logo
-    image_tag("logo.png", :alt => "Rails Tutorial", :class => "round")
+    image_tag("logo.png", :alt => "Rails Tutorial", :class => "round") 
   end
 end
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -16,8 +16,29 @@ describe "LayoutLinks" do
         response.should have_selector('title', :content => 'About')
       end
       
-       it "should have a help page page at 'contact'" do
-          get 'pages/help'
+      it "should have a help page page at 'help'" do
+          get '/help'
           response.should have_selector('title', :content => 'Help')
         end
+        
+      it "should have a sign up page at '/signup'" do
+          get '/signup'
+          response.should have_selector('title', :content => 'Sign up')
+        end
+      
+      it "should have the right links on the layout" do
+        visit root_path
+        response.should have_selector("title", :content => "Home")
+        click_link "About"
+        response.should have_selector("title", :content => "About")
+        click_link "Contact"
+        response.should have_selector("title", :content => "Contact")
+        click_link "Home"
+        response.should have_selector("title", :content => "Home")
+        click_link "Sign up now!"
+        response.should have_selector("title", :content => "Sign up")
+        
+        
+      end
+      
 end
